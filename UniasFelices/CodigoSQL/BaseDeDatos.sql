22cde70fc5df7217caa68b7c8ee727dd8d76d250
Cristian Camilo Morales Jiménez <ccmj202@gmail.com>
2013-11-23 13:48:39 -0500

    ahí arranque con unas cositas

---
+++ UniasFelices/CodigoSQL/BaseDeDatos.sql
@@ -0,0 +1,12 @@
+------------------- Creación de tablas ------------------
+
+CREATE TABLE Jefe(
+cedula number(10) primary key,
+nombre varchar2(15) not null,
+telefono varchar2(10),
+direccion varchar2(20) not null
+);
+
+CREATE TABLE manicurista(
+
+);

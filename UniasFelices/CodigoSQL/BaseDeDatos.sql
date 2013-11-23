71281528f53bd9a601ac588fcd72ab9d07237399
Cristian Camilo Morales Jiménez <ccmj202@gmail.com>
2013-11-23 18:32:46 -0500

    Revisar Anotaciones.txt, Modelo Verbal y carpeta códigos SQL

--- UniasFelices/CodigoSQL/BaseDeDatos.sql
+++ UniasFelices/CodigoSQL/BaseDeDatos.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE Jefe(
 cedula number(10) primary key,
-nombre varchar2(15) not null,
+nombre varchar2(50) not null,
 telefono varchar2(10),
 direccion varchar2(20) not null
 );
@@ -10,7 +10,7 @@ direccion varchar2(20) not null
 CREATE TABLE manicurista(
 jefe number(10) references jefe,
 cedula number(10) primary key,
-nombre varchar2(15) not null,
+nombre varchar2(50) not null,
 telefono varchar2(10),
 direccion varchar2(20) not null,
 nivelEstudio varchar2(10) not null
@@ -20,4 +20,110 @@ CREATE TABLE carrito(
 manicurista number(10) references manicurista,
 cod number(4) primary key,
 color varchar2(15) not null
+);
+
+CREATE TABLE cliente(
+cedula number(10) primary key,
+nombre varchar2(50) not null,
+telefono varchar2(10),
+direccion varchar2(20) not null
+);
+
+------------------------------------------------------------
+--Para Oracle
+CREATE TABLE cita(
+cliente number(10) references cliente,
+fechaHora date primary key,
+tipo varchar2(1) not null check(tipo='d' or tipo = 'l')
+);
+CREATE TABLE domicilio(
+manicurista references manicurista,
+domFecHora DATE references cita,
+direccion varchar2(20) not null,
+primary key(manicurista,domFecHora)
+);
+CREATE TABLE spa(
+manicurista references manicurista,
+spaFecHora DATE references cita,
+primary key(manicurista,spaFecHora)
+);
+-- Para MySql
+CREATE TABLE cita(
+cliente number(10) references cliente,
+fechaHora datetime primary key,
+tipo varchar2(1) not null
+);
+
+CREATE TABLE domicilio(
+manicurista references manicurista,
+domFecHora DATETIME references cita,
+direccion varchar2(20) not null,
+primary key(manicurista,domFecHora)
+);
+CREATE TABLE spa(
+manicurista references manicurista,
+spaFecHora DATETIME references cita,
+primary key(manicurista,spaFecHora)
+);
+--------------------------------------------------------------
+
+CREATE TABLE servicio(
+codigo number(3) primary key,
+precio number(4) not null,
+nombre varchar2(50) not null,
+descripcion varchar2(500)
+);
+
+CREATE TABLE servicioXcita(
+cita DATE references cita,
+service number(3) references servicio,
+primary key(cita,service) 
+);
+
+CREATE TABLE compra(
+consecutivo number(4) primary key,
+fecha DATE not null,
+comprador number(10) references cliente
+);
+
+------------------------------------------------------------------
+----Oracle
+CREATE TABLE item(
+codigo number(4) primary key,
+nombre varchar2(40) not null,
+precio_compra number(6) not null,
+tipo varchar2(1) not null check(tipo='p' or tipo = 'h')
+);
+
+-- MySQL
+CREATE TABLE item(
+codigo number(4) primary key,
+nombre varchar2(40) not null,
+precio_compra number(6) not null,
+tipo varchar2(1) not null
+);
+
+---- Subtipos
+CREATE TABLE producto(
+codprod number(4) primary key references item,
+precio_venta number(4) not null
+);
+
+CREATE TABLE herramienta(
+codherr number(4) primary key references item
+);
+------------------------------------------------------------------
+
+CREATE TABLE productoXcompra(
+codigo_producto_compra number(4) primary key,
+producto number(4) references producto,
+consecutivo number(4) references compra,
+cantidad number(3) not null
+);
+
+CREATE TABLE itemXcarrito(
+cod_carrito number(4) references carrito,
+cod_herramienta number(4) references herramienta,
+cantidad number(2) not null,
+PRIMARY KEY(cod_carrito,cod_herramienta)
 );

f8cf76e10ad0bfd3548e69a2ce51b6d26b9d8eb1
Cristian Camilo Morales Jiménez <ccmj202@gmail.com>
2013-11-23 14:08:27 -0500

    ahí arranque con unas cositas

--- UniasFelices/CodigoSQL/BaseDeDatos.sql
+++ UniasFelices/CodigoSQL/BaseDeDatos.sql
@@ -8,5 +8,16 @@ direccion varchar2(20) not null
 );
 
 CREATE TABLE manicurista(
+jefe number(10) references jefe,
+cedula number(10) primary key,
+nombre varchar2(15) not null,
+telefono varchar2(10),
+direccion varchar2(20) not null,
+nivelEstudio varchar2(10) not null
+);
+
+CREATE TABLE carrito(
+manicurista number(10) references manicurista,
+codigo number(4) primary key,
 
 );

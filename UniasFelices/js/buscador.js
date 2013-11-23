06de7b833deb25b5ae37dcd2bc41b354f9b5fecf
jhta <jeisonhs93@gmail.com>
2013-11-23 01:43:53 -0500

    adelanto un poqito de css de prueba, obviamento esto se hace a lo ultimo

---
+++ UniasFelices/js/buscador.js
@@ -0,0 +1,17 @@
+$(document).on("ready", main);
+
+
+function main(){
+	
+
+
+	$('#barraBusqueda input').keydown(function(event) {
+		//alert("hola3423");
+
+
+		if(event.keyCode==13){
+			alert("hola3423");
+			$('#formBusqueda').submit();
+		}
+	});	
+}
